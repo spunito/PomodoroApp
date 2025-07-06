a610e866558fd5483ff78f6e639e0cc3d5bf67af
Axel Munoz <naclif30@gmail.com>
2025-07-06 01:07:06 -0400

    General Changes timer working first esqueleton made

--- src/pomodoro/PomodoroApp.jsx
+++ src/pomodoro/PomodoroApp.jsx
@@ -1,19 +1,72 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export const PomodoroApp = () => {
   
-    const now = new Date() 
-    now.setMinutes(now.getMinutes() + 25) 
-    console.log(now) 
     
-    
+    const [timer, setTimer] = useState(1500)
+    const [isRunning, setIsRunning] = useState(true)
 
+    const timerRef = useRef(null);
+    const minutos = Math.floor(timer/60)
+    const segundos = timer % 60
+    const txtMinutos = minutos.toString().padStart(2, '0')+ ":" + segundos.toString().padStart(2, '0')
+    const handleReset = () => {
+        setTimer(1500)
+        setIsRunning(false);
+    }
+
+    const handleRunning = () => {
+        setIsRunning(true)
+    }
+    const handleNotRunning = () => {
+        setIsRunning(false)
+    }
+    
+    useEffect(() => {
+        if(isRunning){
+            timerRef.current = setInterval(() => {
+            setTimer(prev => prev - 1 )
+        }, 1000);
+        }
+      return () => {
+        clearInterval(timerRef.current);
+      }
+    }, [isRunning])
+    
+    useEffect(() => {
+      if(timer === 0) {
+        clearInterval(timerRef.current);
+        setIsRunning(false)
+      }
+      
+    }, [timer])
+    
     return (
-    <div className='w-full h-screen flex items-center justify-center bg-gray-100'>
+    <div className='w-full h-screen flex flex-col items-center justify-center bg-gray-800'>
+        <p>{txtMinutos}</p>
+        
+        <div className='mt-4'>
+            <button 
+            id='btnReanude'
+            className='bg-red-800 rounded p-1 px-3 mr-2 cursor-pointer'
+            onClick={handleRunning}>
+                Reanudar
+            </button>
 
-        <a href="">25 Minutos</a>
-        <button className=''></button>
-        <p>{JSON.stringify(now , 3) }</p>
+            <button 
+                className='bg-red-800 rounded p-1 px-3 mr-2 cursor-pointer'
+                onClick={handleReset}>
+                Resetear
+            </button>
+
+            <button 
+                className='bg-red-500 rounded p-1 px-3 cursor-pointer'
+                onClick={handleNotRunning}>
+                Pausar
+            </button>
+        </div>
+        
     </div>
+    
   )
 }
